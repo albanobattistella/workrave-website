35b6fef6f0f8ad60877ad3a76c26dbcef361bf69
rcaelers <rcaelers@users.noreply.github.com>
2020-04-04 21:43:35 +0000

    deploy: 6da50f43154cae9fafe8529e7ceb8087f213f7be

---
+++ js/main.min.5480dddda06ff5620b45324c25b188c4bb9d9b51c2e8d2fdadf3c658a4f5465439701cff4c60614105add9d4fd70b4594d963a96546fae1428c4cd38131edbdb.js
@@ -0,0 +1,4 @@
+var scrollTop=$(window).scrollTop();if(scrollTop>20){$(".navbar").addClass("scrolled");$(".navbar").addClass("hidden");$(".navbar").addClass("notransition");}
+$(document).ready(function(e){var _previousScrollTop=0;function checkScroll(){var scrollTop=$(window).scrollTop();if(scrollTop>20){$(".navbar").addClass("scrolled");var scrollDelta=scrollTop-_previousScrollTop;_previousScrollTop=scrollTop;if(scrollDelta<0){$(".navbar").removeClass("hidden");}else if(scrollDelta>0){$(".navbar").addClass("hidden");try{$(".dropdown.show .dropdown-toggle",$(".navbar")).dropdown("toggle");}catch(e){}}}else{$(".navbar").removeClass("scrolled");$(".navbar").removeClass("hidden");$(".navbar").removeClass("notransition");}}
+if($(".navbar").length>0){var scrollTop=$(window).scrollTop();if(scrollTop>20){$(".navbar").addClass("scrolled");$(".navbar").addClass("hidden");$(".navbar").addClass("notransition");}
+$(window).on("scroll load resize",function(){checkScroll();});}});
